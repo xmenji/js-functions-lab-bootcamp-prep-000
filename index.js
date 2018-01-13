2a7e82cfd5065427df74460604b963b1b592b6ac
Xochitl Menjivar <xandra.menji@gmail.com>
2018-01-13 01:44:33 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,3 +7,10 @@ function happyHolidaysTo(name){
   return "Happy holidays, ${name}";
 }
 
+function happyHolidayTo(holiday, name){
+  return "Happy ${holiday}, ${name}";
+}
+
+function holidayCountdown(holiday, days){
+  return "It's ${days} days until ${holiday}!";
+}
